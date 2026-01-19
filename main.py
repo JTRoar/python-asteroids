0913c9951eef49b6d89efb9c3fbdcb27450ff1fc
JTRoar <JohnTigerRoar@gmail.com>
2026-01-19 16:09:59 -0500

    Updated player class to rotate left and right

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock() #clock object
     dt = 0
+    #use the player class created in player.py
     player_object = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2)) 
     
     while True:
@@ -25,12 +26,13 @@ def main():
         #Game logic and drawing goes here
         screen.fill("black") #set background screen to black
         
-        #draw player
+        #draw player from parameters passed in player_object, which gets all the goods from Player class and inherited CircleShape parent
         player_object.draw(screen)
         #Call .tick(60) at the end of the loop and divide return value by 1000 to get ms, and save to dt "Delta time"
         dt = clock.tick(60) / 1000 #set the FPS to 60
         #print(dt) // may be useful to have in future
-
+        #rotate the player object left and right
+        player_object.update(dt) 
         pygame.display.flip() #refreshes the screen, always call last
         
 
