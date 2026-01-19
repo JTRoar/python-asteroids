55523bf6ff3f1c4d49166a866ee4ebf4f993b02e
JTRoar <JohnTigerRoar@gmail.com>
2026-01-19 18:45:50 -0500

    Added collision detection between player ship and asteroids

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+import sys
 from constants import *
 from logger import log_state
+from logger import log_event
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 
 def main():
@@ -18,18 +22,33 @@ def main():
     #GROUPS
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     #Player is name of class, not an instance of it
     #this must be done before any player objects are created
     Player.containers = (
         updatable, #all the objects that can be updated
         drawable   #all the objects that can be drawn
         )
+    
+    Asteroid.containers = (
+        asteroids,
+        updatable,
+        drawable,
+    )
+    #create asteroid field container, only add to updatable
+    AsteroidField.containers = updatable 
+
+    #create new asteroidfield object
+    asteroid_field = AsteroidField()
+
+
 
     #use the player class created in player.py
     player_object = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2)) 
     
     while True:
         log_state()
+        log_event("player_hit")
         for event in pygame.event.get():
             if event.type == pygame.QUIT: ##allow you to exit out by clicking X
                 return
@@ -37,15 +56,19 @@ def main():
         #Game logic and drawing goes here 
         screen.fill("black") #set background screen to black
         
+        updatable.update(dt) ## same as the for loop i was doing, but how it was meant to be written?
+        
+        for asteroid in asteroids:
+            if asteroid.collides_with(player_object):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        
         for object in drawable:  #draw all the drawables
             object.draw(screen)
         
         #Call .tick(60) at the end of the loop and divide return value by 1000 to get ms, and save to dt "Delta time"
         dt = clock.tick(60) / 1000 #set the FPS to 60
-        
-        #rotate the player object left and right
-        for object in updatable:  #update all the updatables
-            object.update(dt) 
         
         pygame.display.flip() #refreshes the screen, always call last
         
