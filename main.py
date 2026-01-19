a98aa06a0d03b5c4624d328945473f67b50a2457
JTRoar <JohnTigerRoar@gmail.com>
2026-01-19 16:42:35 -0500

    Added Groups and containers so we don't have to update and draw each object manually

--- main.py
+++ main.py
@@ -14,6 +14,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock() #clock object
     dt = 0
+    
+    #GROUPS
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    #Player is name of class, not an instance of it
+    #this must be done before any player objects are created
+    Player.containers = (
+        updatable, #all the objects that can be updated
+        drawable   #all the objects that can be drawn
+        )
+
     #use the player class created in player.py
     player_object = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2)) 
     
@@ -23,16 +34,19 @@ def main():
             if event.type == pygame.QUIT: ##allow you to exit out by clicking X
                 return
         
-        #Game logic and drawing goes here
+        #Game logic and drawing goes here 
         screen.fill("black") #set background screen to black
         
-        #draw player from parameters passed in player_object, which gets all the goods from Player class and inherited CircleShape parent
-        player_object.draw(screen)
+        for object in drawable:  #draw all the drawables
+            object.draw(screen)
+        
         #Call .tick(60) at the end of the loop and divide return value by 1000 to get ms, and save to dt "Delta time"
         dt = clock.tick(60) / 1000 #set the FPS to 60
-        #print(dt) // may be useful to have in future
+        
         #rotate the player object left and right
-        player_object.update(dt) 
+        for object in updatable:  #update all the updatables
+            object.update(dt) 
+        
         pygame.display.flip() #refreshes the screen, always call last
         
 
